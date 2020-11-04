fbacecd6f24e486a57245bd58a62eff951ed4d8c
Daniel Almeida <daniel.mag.almeida@gmail.com>
2020-11-04 21:54:39 +0000

    Backend project created

---
+++ backend/src/main/java/com/isdma/dslearnbdsd/DslearnbdsdApplication.java
@@ -0,0 +1,13 @@
+package com.isdma.dslearnbdsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnbdsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnbdsdApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/isdma/dslearnbdsd/DslearnbdsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isdma.dslearnbdsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnbdsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
