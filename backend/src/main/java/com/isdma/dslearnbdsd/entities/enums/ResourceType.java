ffc11ab36931d2f656fdcc30f4545346b15e260f
Daniel Almeida <daniel.mag.almeida@gmail.com>
2020-11-05 16:28:17 +0000

    Resource

---
+++ backend/src/main/java/com/isdma/dslearnbdsd/entities/enums/ResourceType.java
@@ -0,0 +1,6 @@
+package com.isdma.dslearnbdsd.entities.enums;
+
+public enum ResourceType {
+
+	LESSON_ONLY, LESSON_TASK, FORUM, EXTERNAL_LINK;
+}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ( 'Trilha HTML', 'Trilha principal do curso', 1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ1vGLoOegt5KuNntjFXvFhTDTxxY1I2ycnxw&usqp=CAU', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ( 'Forum', 'Tire suas duvidas', 2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ1vGLoOegt5KuNntjFXvFhTDTxxY1I2ycnxw&usqp=CAU', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ( 'Lives', 'Lives Exclusivas para a turma', 3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ1vGLoOegt5KuNntjFXvFhTDTxxY1I2ycnxw&usqp=CAU', 0, 1);
